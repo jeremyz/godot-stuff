d231a3807a401082d292ee076448a679dbac9c8a
Jérémy Zurcher <jeremy@asynk.ch>
2019-02-05 00:54:37 +0100

    boardgame : clean up unit

--- boardgame/unit/unit.gd
+++ boardgame/unit/unit.gd
@@ -4,44 +4,52 @@ extends Area2D
 
 export(Texture) var sprite
 
-var mouseIn = false
+var mouse_in = false
 var dragging = false
-var back = false
+var reverse = false
 var dying = false
-var deathTime = 0
-var _selected
-var _hit
-var _drop_zone
+var death_time = 0
+var drop_zone = null
+
+onready var selected_tex = load("res://unit/02_selected.png")
+onready var hit_tex = load("res://unit/03_hit.png")
+onready var tween = $Tween
+onready var overlay = $Overlay
 
 func _ready():
 	$Sprite.texture = sprite
-	_selected = load("res://unit/02_selected.png")
-	_hit = load("res://unit/03_hit.png")
-	$Tween.connect("tween_completed", self, "_on_tween_completed")
+	tween.connect("tween_completed", self, "_on_tween_completed")
 	connect("mouse_entered", self, "_on_mouse_entered")
 	connect("mouse_exited", self, "_on_mouse_exited")
 	connect("area_entered", self, "_on_area_entered")
-	connect("area_exited", self, "_on_area_exited")
 
-func kill(): dying = true
-func set_spent(v): $Spent.visible = v
-func is_spent(): return $Spent.visible
+func kill():
+	dying = true
+
+func set_spent(v):
+	$Spent.visible = v
+
 func hit():
-	$Selected.texture = _hit
-	$Selected.visible = true
-	_do_hit(0, 0.6, 0.2, true)
+	overlay.texture = hit_tex
+	overlay.visible = true
+	_do_hit(0.6, 0.2, true)
 
-func _do_hit(a, b, c, d):
-	back = d
-	$Tween.interpolate_method(self, "__apply", a, b, c, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-	$Tween.start()
-func __apply(v): $Selected.modulate.a = v
+func _do_hit(to, time, blink):
+	reverse = blink
+	var v0 = overlay.modulate
+	var v1 = overlay.modulate
+	v1.a = to
+	tween.interpolate_property(overlay, "modulate", v0, v1, time, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+
 func _on_tween_completed(a, b):
-	if back: _do_hit(.6, 0, 0.2, false)
-	else: $Selected.visible = false
+	if reverse:
+		_do_hit(0, 0.2, false)
+	else:
+		overlay.visible = false
 
 func _input(event):
-	if mouseIn && event is InputEventMouseButton && event.is_pressed():
+	if mouse_in && event is InputEventMouseButton && event.is_pressed():
 		rotation = 0
 		dragging = true
 		_set_selected(true)
@@ -52,32 +60,37 @@ func _input(event):
 		else:
 			dragging = false
 			_set_selected(false)
-			if _drop_zone:
-				position = _drop_zone.position
-				rotation = _drop_zone.rotation
+			if drop_zone and overlaps_area(drop_zone):
+				position = drop_zone.position
+				rotation = drop_zone.rotation
+			else:
+				drop_zone = null
 		get_tree().set_input_as_handled()
 
-func _process(delta): if dying: _die()
+func _process(delta):
+	if dying:
+		_die()
 
 func _set_selected(v):
-	$Selected.texture = _selected
-	$Selected.visible = v
-	$Selected.modulate.a = 1
+	overlay.texture = selected_tex
+	overlay.visible = v
+	overlay.modulate.a = 1
 
 func _die():
-	deathTime += 0.025
-	if deathTime >= 4.0:
-		deathTime = 0
+	death_time += 0.025
+	if death_time >= 4.0:
+		death_time = 0
 		dying = false
-	$Spent.modulate.a = (1 - deathTime / 2.0)
-	$Pawn.material.set_shader_param("time", deathTime)
-	$Sprite.material.set_shader_param("amount", pow(deathTime, 4)) 
+	$Spent.modulate.a = (1 - death_time / 2.0)
+	$Pawn.material.set_shader_param("time", death_time)
+	$Sprite.material.set_shader_param("amount", pow(death_time, 4))
 
-func _on_mouse_entered(): mouseIn = true
-func _on_mouse_exited():  mouseIn = false
-func _on_area_exited(a):
-	if a == _drop_zone:
-		_drop_zone = null
+func _on_mouse_entered():
+	mouse_in = true
+
+func _on_mouse_exited():
+	mouse_in = false
+
 func _on_area_entered(a):
 	if a.is_in_group("drop_zone"):
-		_drop_zone = a
+		drop_zone = a
