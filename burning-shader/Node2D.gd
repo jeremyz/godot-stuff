d441035c58bbb51f66d378f62a02443b480ffff7
Jérémy Zurcher <jeremy@asynk.ch>
2021-03-23 10:37:36 +0100

    burning-shader : code cleanup

--- burning-shader/Node2D.gd
+++ burning-shader/Node2D.gd
@@ -5,18 +5,20 @@ var time = 0.0;
 var num = 1;
 
 func _ready():
-	for n in range(4): img.append(load("res://page%d.png" % n))
+	for n in range(4):
+		img.append(load("res://page%d.png" % n))
 	$TextureRect.material.set_shader_param("tex1", img[0])
+	$TextureRect.material.set_shader_param("tex2", img[1])
 
-func math():
+func math(delta : float):
 	$TextureRect.material.set_shader_param("time", time)
-	time += 0.01
-	if time == 0.01:
-		if num == img.size() - 1: num = 0
+	if time == 0:
 		$TextureRect.material.set_shader_param("tex2", img[num])
+	time += delta
 	if time > 3.0:
 		$TextureRect.material.set_shader_param("tex1", img[num])
-		time = 0.0
-		num += 1;
+		num = (num + 1) % img.size()
+		time = 0
 
-func _process(delta): math()
+func _process(delta : float):
+	math(delta)
