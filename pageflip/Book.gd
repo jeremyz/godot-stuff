b0c7802a2d6141a85f1d8edf4c568cc1ca9e9c66
Jérémy Zurcher <jeremy@asynk.ch>
2025-01-13 12:13:18 +0100

    pageflip : migrate to godot 4.4 & fix anchors

--- pageflip/Book.gd
+++ pageflip/Book.gd
@@ -19,18 +19,27 @@ class_name Book
 func _ready() -> void:
 	slider.visible = false
 	slider.self_modulate.a = 0.5
-	page.material.set_shader_parameter("time", 0)
-	page.material.set_shader_parameter("rect", page.size)
-	page.material.set_shader_parameter("flip_duration", flip_duration)
-	page.material.set_shader_parameter("cylinder_ratio", cylinder_ratio)
-	$Clipper/Left.size.x = border_width
 	$Clipper/Left.gui_input.connect(_on_slider_input.bind(true))
 	$Clipper/Left.mouse_entered.connect(_on_slider.bind(true, true))
 	$Clipper/Left.mouse_exited.connect(_on_slider.bind(true, false))
-	$Clipper/Right.size.x = border_width
+	$Clipper/Left.offset_right = border_width
 	$Clipper/Right.gui_input.connect(_on_slider_input.bind(false))
 	$Clipper/Right.mouse_entered.connect(_on_slider.bind(false, true))
 	$Clipper/Right.mouse_exited.connect(_on_slider.bind(false, false))
+	$Clipper/Right.offset_left = -border_width
+
+func _reset_shader(flip : bool) -> void:
+	time = 0.0
+	flipping = flip
+	var flip_left = pages[0] < pages[1]
+	page.material.set_shader_parameter("time", time)
+	page.material.set_shader_parameter("rect", page.size)
+	page.material.set_shader_parameter("flip_duration", flip_duration)
+	page.material.set_shader_parameter("cylinder_ratio", cylinder_ratio)
+	page.material.set_shader_parameter("flip_left", flip_left)
+	page.material.set_shader_parameter("cylinder_direction", Vector2(5.0, 1.0 if flip_left else -1.0))
+	page.material.set_shader_parameter("current_page", load("res://%s/%02d.png" % [pages_dir, pages[0]]))
+	page.material.set_shader_parameter("next_page", load("res://%s/%02d.png" % [pages_dir, pages[1]]))
 
 func show_book(dir : String, current_page : int, max_page : int) -> void:
 	pages_dir = dir
@@ -69,15 +78,6 @@ func _on_prev() -> void:
 func _on_next() -> void:
 	pages[1] = pages[0] + 1
 	_reset_shader(true)
-
-func _reset_shader(flip : bool) -> void:
-	time = 0.0
-	flipping = flip
-	var flip_left = pages[0] < pages[1]
-	page.material.set_shader_parameter("flip_left", flip_left)
-	page.material.set_shader_parameter("cylinder_direction", Vector2(5.0, 1.0 if flip_left else -1.0))
-	page.material.set_shader_parameter("current_page", load("res://%s/%02d.png" % [pages_dir, pages[0]]))
-	page.material.set_shader_parameter("next_page", load("res://%s/%02d.png" % [pages_dir, pages[1]]))
 
 func _on_slider(left : bool, entered : bool) -> void:
 	if (left and pages[0] == 0) or (not left and pages[0] == pages[2]):
